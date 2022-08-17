7e28753bfcc234dab28f656deeb5fc94451ed69d
Natalie  Narkprasert <natalie.nark@gmail.com>
2022-08-17 16:17:56 +0100

    D

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+# Defines the root path route ("/")
+# root "articles#index"
+
+Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+    # collection do
+    #   get :top
+    # end
+    # member do
+    #   get :chef
+    # end
+  end
+
+  resources :reviews, only: %i[show edit update destroy]
 end
